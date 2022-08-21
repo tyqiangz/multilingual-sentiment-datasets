7e96c5ab0df05ae3b591444ba1ad6302217e7a73
tyqiangz <40680129+tyqiangz@users.noreply.github.com>
2022-08-21 23:02:27 +0800

    uploaded english and indonesian datasets

---
+++ retrieve_data_scripts/xlm_t.py
@@ -0,0 +1,23 @@
+import requests
+
+LANGUAGES = {"english": "en"}
+SPLITS = ["train", "val", "test"]
+ID2LABEL = {"0": "negative", "1": "neutral", "2": "positive"}
+
+for language in LANGUAGES:
+    for split in SPLITS:
+        text_url = f"https://raw.githubusercontent.com/cardiffnlp/xlm-t/main/data/sentiment/{language}/{split}_text.txt"
+        label_url = f"https://raw.githubusercontent.com/cardiffnlp/xlm-t/main/data/sentiment/{language}/{split}_labels.txt"
+
+        text_response = requests.get(text_url)
+        texts = text_response.content.decode('unicode-escape').split("\n")
+
+        label_response = requests.get(label_url)
+        labels = label_response.content.decode('unicode-escape').split("\n")
+
+        assert len(texts) == len(
+            labels), f"Number of texts and labels not equal for {language}/{split}_text.txt and {language}/{split}_labels.txt"
+
+        with open(f"../{LANGUAGES[language]}/{split}.tsv", "w") as f:
+            for text, label in zip(texts, labels):
+                f.write(text + "\t" + ID2LABEL[label] + "\n")
