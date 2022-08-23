a7caec570e97d91dd8012e97d7ed3a8dfce0d739
tyqiangz <40680129+tyqiangz@users.noreply.github.com>
2022-08-23 23:06:31 +0800

    added a folder that combines all languages

---
+++ retrieve_data_scripts/combine_all.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+LANGUAGES = [
+    'malay', 'hindi', 'japanese', 'german',
+    'italian', 'english', 'portuguese', 'french',
+    'spanish', 'chinese', 'indonesian', 'arabic'
+]
+
+df_list = {"train": [], "valid": [], "test": []}
+
+for lang in LANGUAGES:
+    for split in ["train", "valid", "test"]:
+        print(lang, split)
+        filepath = f"../data/{lang}/{split}.tsv"
+        df = pd.read_csv(filepath, sep='\t')
+        df["language"] = lang
+        df_list[split].append(df)
+
+train_df = pd.concat(df_list["train"])
+valid_df = pd.concat(df_list["valid"])
+test_df = pd.concat(df_list["test"])
+
+train_df.to_csv("../data/all/train.tsv", sep="\t", index=False)
+valid_df.to_csv("../data/all/valid.tsv", sep="\t", index=False)
+test_df.to_csv("../data/all/test.tsv", sep="\t", index=False)
