193901ee19f37b1e6a70d069f1e46c7bcba96503
tyqiangz <40680129+tyqiangz@users.noreply.github.com>
2022-08-23 23:49:49 +0800

    corrected malay test set duplication

--- retrieve_data_scripts/malaya.py
+++ retrieve_data_scripts/malaya.py
@@ -14,18 +14,18 @@ dfs = [
 ]
 
 texts = []
-labels= []
+labels = []
 
 for i in range(len(dfs[0])):
     if not pd.isna(dfs[0].loc[i, "label"]):
         texts.append(dfs[0].loc[i, "text"])
         labels.append(dfs[0].loc[i, "label"])
-    
+
 for i in range(len(dfs[1])):
     if not pd.isna(dfs[1].loc[i, "sentiment"]):
         texts.append(dfs[1].loc[i, "text"])
         labels.append(dfs[1].loc[i, "sentiment"])
-    
+
 for i in range(len(dfs[2])):
     if not pd.isna(dfs[2].loc[i, "sentiment"]):
         texts.append(dfs[2].loc[i, "text"])
@@ -40,19 +40,23 @@ y = df["label"]
 sss1 = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=0)
 
 for train_index, dev_index in sss1.split(X, y):
-    X_train, X_dev = X[train_index].reset_index(drop=True), X[dev_index].reset_index(drop=True)
-    y_train, y_dev = y[train_index].reset_index(drop=True), y[dev_index].reset_index(drop=True)
+    X_train, X_dev = X[train_index].reset_index(
+        drop=True), X[dev_index].reset_index(drop=True)
+    y_train, y_dev = y[train_index].reset_index(
+        drop=True), y[dev_index].reset_index(drop=True)
 
 sss2 = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=0)
 
 for val_index, test_index in sss2.split(X_dev, y_dev):
-    X_val, X_test = X_dev[val_index].reset_index(drop=True), X_dev[test_index].reset_index(drop=True)
-    y_val, y_test = y_dev[val_index].reset_index(drop=True), y_dev[test_index].reset_index(drop=True)
-    
+    X_val, X_test = X_dev[val_index].reset_index(
+        drop=True), X_dev[test_index].reset_index(drop=True)
+    y_val, y_test = y_dev[val_index].reset_index(
+        drop=True), y_dev[test_index].reset_index(drop=True)
+
 train_df = pd.concat([X_train, y_train], axis=1).reset_index(drop=True)
 valid_df = pd.concat([X_val, y_val], axis=1).reset_index(drop=True)
 test_df = pd.concat([X_test, y_test], axis=1).reset_index(drop=True)
 
-train_df.to_csv("../data/ms/train.tsv", sep="\t", index=False)
-valid_df.to_csv("../data/ms/valid.tsv", sep="\t", index=False)
-valid_df.to_csv("../data/ms/test.tsv", sep="\t", index=False)
+train_df.to_csv("../data/malay/train.tsv", sep="\t", index=False)
+valid_df.to_csv("../data/malay/valid.tsv", sep="\t", index=False)
+test_df.to_csv("../data/malay/test.tsv", sep="\t", index=False)
