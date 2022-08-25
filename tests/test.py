0f4d140251eda600db2be6ecd8f92dce454be2b7
tyqiangz <tay.yongqiang@gmail.com>
2022-08-25 17:47:02 +0800

    added data source column to all dataset splits

--- tests/test.py
+++ tests/test.py
@@ -15,10 +15,18 @@ for lang in LANGUAGES:
 
     for split in ['train', 'valid', 'test']:
         df = pd.read_csv(f'../data/{lang}/{split}.csv')
+        # df = pd.read_csv(f"https://raw.githubusercontent.com/tyqiangz/multilingual-sentiment-datasets/main/data/{lang}/{split}.csv")
 
         assert 'text' in df.columns, f"./{lang}/{split}.csv doesn't have 'text' column"
         assert 'label' in df.columns, f"./{lang}/{split}.csv doesn't have 'label' column"
         assert 'source' in df.columns, f"./{lang}/{split}.csv doesn't have 'source' column"
         
+        label_classes = df["label"].unique()
+
+        assert len(label_classes) == 3
+        assert "positive" in label_classes
+        assert "neutral" in label_classes
+        assert "negative" in label_classes
+
         if lang == 'all':
             assert 'language' in df.columns, f"./{lang}/{split}.csv doesn't have 'language' column"
