8dfd8415be4e938f0565c5ae1a03c1284a341404
tyqiangz <tay.yongqiang@gmail.com>
2022-08-25 15:31:13 +0800

    added tests to check the format of all datafiles

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+""" test tthat all the data files conform to some common format """
+import pandas as pd
+import os
+
+LANGUAGES = os.listdir("../data")
+
+assert "all" in LANGUAGES
+
+for lang in LANGUAGES:
+    filenames = os.listdir(f'../data/{lang}')
+
+    assert 'train.csv' in filenames, f"./{lang} doesn't have 'train.csv'"
+    assert 'valid.csv' in filenames, f"./{lang} doesn't have 'valid.csv'"
+    assert 'test.csv' in filenames, f"./{lang} doesn't have 'test.csv'"
+
+    for split in ['train', 'valid', 'test']:
+        df = pd.read_csv(f'../data/{lang}/{split}.csv')
+
+        assert 'text' in df.columns, f"./{lang}/{split}.csv doesn't have 'text' column"
+        assert 'label' in df.columns, f"./{lang}/{split}.csv doesn't have 'label' column"
+        assert 'source' in df.columns, f"./{lang}/{split}.csv doesn't have 'source' column"
+        
+        if lang == 'all':
+            assert 'language' in df.columns, f"./{lang}/{split}.csv doesn't have 'language' column"
